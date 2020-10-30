aa47c4b3e71780664d22416c6db7707e6fa721d5
Hatooz <hatem.mohieldin@gmail.com>
2020-10-30 09:53:58 +0100

    kmom04 testing to backend, added reset db script to pretest in package.json

--- db/migrate.sql
+++ db/migrate.sql
@@ -18,4 +18,5 @@ CREATE TABLE IF NOT EXISTS texts (
 );
 
 INSERT INTO info (aboutme)
-VALUES ("Lite info om mig. Jag är student här på BTH webbprogrammering och har börjat mitt andra år nu på distans. Denna kursen ser väldigt spännande ut med begrepp som devOps och websockets. Dessa har man hört om men aldrig förstått innebörden av så jag ser fram emot att lära mig om dessa bland allt annat.")
+VALUES 
+    ("Lite info om mig. Jag är student här på BTH webbprogrammering och har börjat mitt andra år nu på distans. Denna kursen ser väldigt spännande ut med begrepp som devOps och websockets. Dessa har man hört om men aldrig förstått innebörden av så jag ser fram emot att lära mig om dessa bland allt annat.")
